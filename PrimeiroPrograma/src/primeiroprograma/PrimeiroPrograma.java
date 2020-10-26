84b4f1fd1cc13cd9a19b787a23ce8156b2a14749
Felipe Baz <54612024+Felipe-Baz@users.noreply.github.com>
2020-10-26 16:10:21 -0300

    atualização do PrimeiroPrograma
    um rapida atualização no codigo do PrimeiroPrograma

--- PrimeiroPrograma/src/primeiroprograma/PrimeiroPrograma.java
+++ PrimeiroPrograma/src/primeiroprograma/PrimeiroPrograma.java
@@ -1,15 +1,25 @@
 package primeiroprograma;
 
 /**
- *
  * @author Felipe Baz Mitsuishi
+ * 
+ * Teoria:
+ * MinhaClasse = classe ou interface
+ * meuAtributo = atributo
+ * minhaVariavel = variavel
+ * meuMetodo = metodo
+ * meu_pacote = pacote
+ * MINHA_CTE = constante
  */
 public class PrimeiroPrograma {
 
     /**
      * @param args the command line arguments
      */
+    //psvm = public static void main
     public static void main(String[] args) {
-    System.out.print("Ola, mundo!\n");
+    System.out.print("Ola, mundo!\n "); // System.out.print("Ola, mundo!\n ") = System.out.println("Ola, mundo!");
+    //sout = System.out.println("");
+        
     }
 }
