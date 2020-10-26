ebdcd6b3c86e518c3463a4c90ee137ee63587015
Felipe Baz <54612024+Felipe-Baz@users.noreply.github.com>
2020-10-26 14:41:28 -0300

    Primeiro Programa
    Consta de um programa simples de impressão no terminal java da frase "Ola, mundo!", alem de conter a teoria sobre camel case.

---
+++ PrimeiroPrograma/src/primeiroprograma/PrimeiroPrograma.java
@@ -0,0 +1,15 @@
+package primeiroprograma;
+
+/**
+ *
+ * @author Felipe Baz Mitsuishi
+ */
+public class PrimeiroPrograma {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+    System.out.print("Ola, mundo!\n");
+    }
+}
