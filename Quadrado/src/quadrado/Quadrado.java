495cc34239b6db40f49c37756d9df47893f0a2d7
Felipe Baz <54612024+Felipe-Baz@users.noreply.github.com>
2020-12-07 23:43:58 -0300

    Quadrados
    É um teste para aprendizado dos metodos contrutores, classes e objetos.

---
+++ Quadrado/src/quadrado/Quadrado.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package quadrado;
+
+/**
+ * @author Felipe Baz Mitsuishi
+ */
+public class Quadrado {
+
+    public static void main(String[] args) {
+        quadradoclasse quadrado1 = new quadradoclasse(Math.random());
+        System.out.println("Area 1: " + quadrado1.area());
+        
+        quadradoclasse quadrado2 = new quadradoclasse(Math.random());
+        System.out.println("Area 2: " + quadrado2.area());
+        
+        quadradoclasse quadrado3 = new quadradoclasse(Math.random());
+        System.out.println("Area 3: " + quadrado3.area());
+        
+        quadradoclasse quadrado4 = new quadradoclasse(Math.random());
+        System.out.println("Area 4: " + quadrado4.area());
+        
+        quadradoclasse quadrado5 = new quadradoclasse(Math.random());
+        System.out.println("Area 5: " + quadrado5.area());
+        
+        quadradoclasse quadrado6 = new quadradoclasse(Math.random());
+        System.out.println("Area 6: " + quadrado6.area());
+        
+        quadradoclasse quadrado7 = new quadradoclasse(Math.random());
+        System.out.println("Area 7: " + quadrado7.area());
+        
+        quadradoclasse quadrado8 = new quadradoclasse(Math.random());
+        System.out.println("Area 8: " + quadrado8.area());
+        
+        quadradoclasse quadrado9 = new quadradoclasse(Math.random());
+        System.out.println("Area 9: " + quadrado9.area());
+        
+        quadradoclasse quadrado10 = new quadradoclasse(Math.random());
+        System.out.println("Area 10: " + quadrado10.area());
+    }
+    
+}
